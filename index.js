7dc25cd148521df3f5c0856b86112792b50743db
JonKnitt1205 <jonknitt1205@gmail.com>
2023-02-04 12:49:10 -0500

    added css and public file structure

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const port = 7777;
 
+// get resouces from /public
+app.use(express.static(__dirname + '/public'));
+
 // send the index.html file to the user on the GET request
 app.get('/', function (req, res){
-    res.sendFile('index.html', {root: __dirname})
+    res.sendFile('index.html', {root: __dirname + '/public/html'})
 });
 
 // listen on the port
