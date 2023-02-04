5442d65ace7097541b56d4f5f92c177455ac040c
JonKnitt1205 <jonknitt1205@gmail.com>
2023-02-04 13:40:44 -0500

    added a textbox that echos to the server console

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 7777;
 
 // get resouces from /public
 app.use(express.static(__dirname + '/public'));
+
+// for the post requests
+app.use(bodyParser.urlencoded({extended:true}))
 
 // send the index.html file to the user on the GET request
 app.get('/', function (req, res){
@@ -13,4 +17,11 @@ app.get('/', function (req, res){
 // listen on the port
 app.listen(port, ()=> {
     console.log(`Now listening on port ${port}`);
+});
+
+// here you can get the value of from the textbox 
+app.post('/',(req,res)=>{
+    let text = req.body.theTextbox; 
+    console.log(text);
+    res.sendFile('index.html', {root: __dirname + '/public/html'});
 });
